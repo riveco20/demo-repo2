ccb76dbed5176d8aef406ce5a37130450d13b829
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 17:24:25 -0500

    Operciones con operadores de Asignacion y composicion

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/EjerciciosOperadores/OperadoresAsignacion.java
@@ -0,0 +1,17 @@
+package com.ejercicioCadena.EjerciciosConCadenas.EjerciciosOperadores;
+
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+        //asignacion en java
+        int a=2, b=2;
+        //Operador de composicion 
+        a+=1;
+        System.out.println("a = " + a);
+        a+=3;
+        System.out.println("a = " + a);
+        b-=1;
+        System.out.println("b = " + b);
+        b/=2;
+        System.out.println("b = " + b);
+    }
+}
