2a251d0a7a8d3eb667573e99e872952d0d324cba
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 15:11:53 -0500

    Agregando primer ejercicion con cadena

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/EjerciciosConCadenasApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicioCadena.EjerciciosConCadenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosConCadenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosConCadenasApplication.class, args);
+	}
+
+}
---
+++ Ejercicio1/EjerciciosConCadenas/src/test/java/com/ejercicioCadena/EjerciciosConCadenas/EjerciciosConCadenasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicioCadena.EjerciciosConCadenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosConCadenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
