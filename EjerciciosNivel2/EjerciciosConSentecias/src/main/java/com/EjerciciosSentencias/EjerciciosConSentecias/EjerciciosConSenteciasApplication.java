016beede763710dffdbb64fe6e5e8a3921031efb
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 17:49:32 -0500

    Se agrego proyecto ejercicios nivel 2 y se modifico el nombre de ejercicio1

---
+++ EjerciciosNivel2/EjerciciosConSentecias/src/main/java/com/EjerciciosSentencias/EjerciciosConSentecias/EjerciciosConSenteciasApplication.java
@@ -0,0 +1,13 @@
+package com.EjerciciosSentencias.EjerciciosConSentecias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosConSenteciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosConSenteciasApplication.class, args);
+	}
+
+}
---
+++ EjerciciosNivel2/EjerciciosConSentecias/src/test/java/com/EjerciciosSentencias/EjerciciosConSentecias/EjerciciosConSenteciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjerciciosSentencias.EjerciciosConSentecias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosConSenteciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
