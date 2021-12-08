a6ed7dcaf3b47d960d01c9e0aa397f5baa3182fd
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 15:37:08 -0500

    Agregando ejercicion

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/Concatenacion.java
@@ -0,0 +1,9 @@
+package com.ejercicioCadena.EjerciciosConCadenas;
+
+public class Concatenacion {
+    public static void main(String[] args) {
+        var nombre = "Richard Vellojin";
+        var saludo = "Hola";
+        System.out.println(saludo + " " + nombre + " Como te encuentras hoy");
+    }
+}
