6af822fa2c42ad1704b63e1d37d954266f7ab0c8
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 17:41:34 -0500

    Operaciones con && y ||

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/EjerciciosOperadores/OperadoresCondicionales.java
@@ -0,0 +1,33 @@
+package com.ejercicioCadena.EjerciciosConCadenas.EjerciciosOperadores;
+
+import ch.qos.logback.core.db.BindDataSourceToJNDIAction;
+
+public class OperadoresCondicionales {
+    public static void main(String[] args) {
+        int a =3;
+        int valoMinimo =0;
+        int valorMaximo =10;
+        boolean res =a>=valoMinimo && a<= valorMaximo;
+        System.out.println("res = " + res);
+
+        if(a>=valoMinimo && a<= valorMaximo){
+            System.out.println("Dentro de rango");
+        }
+        else{
+            System.out.println("Fuera de rango");
+        }
+
+        //operador ||
+
+        boolean vaciones = false;
+        boolean diaDescanso =false;
+        if(vaciones||diaDescanso){
+            System.out.println("El padre no esta ocupado puede asistir al juego del hijo");
+        }
+        else{
+            System.out.println("El padre esta ocupado no puede asistir al juego del hijo ");
+
+        }
+        
+    }
+}
