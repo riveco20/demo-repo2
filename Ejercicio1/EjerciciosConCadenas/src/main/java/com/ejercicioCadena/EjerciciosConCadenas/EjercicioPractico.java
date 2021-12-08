dde49b032f6b30bce1441c5f0b76ab1c05c7beec
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 15:44:19 -0500

    "Agregando EjercicioPractico con caracteres"

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/EjercicioPractico.java
@@ -0,0 +1,21 @@
+package com.ejercicioCadena.EjerciciosConCadenas;
+
+public class EjercicioPractico {
+    public static void main(String[] args) {
+        String nombre = "Karlas";
+        String apellido = "Esparza";
+        System.out.println(nombre + " "+ apellido);
+
+        System.out.println("nueva linea: \n" + nombre);
+
+        System.out.println("Tabulador: \t" + nombre);
+
+        System.out.println("Retroceso: \b" + nombre);
+
+        System.out.println("Retorno de carro: \r" + nombre);
+
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+
+        System.out.println("Comilla doble: \"" + nombre + " \"");
+    }
+}
