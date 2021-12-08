4459780f100273d4149935386b09142582e6de74
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-08 16:17:44 -0500

    Agregando tipo boolean

---
+++ Ejercicio1/EjerciciosConCadenas/src/main/java/com/ejercicioCadena/EjerciciosConCadenas/EjeciciosNumeros/TipoPrimitvosBoolean.java
@@ -0,0 +1,27 @@
+package com.ejercicioCadena.EjerciciosConCadenas.EjeciciosNumeros;
+
+public class TipoPrimitvosBoolean {
+    public static void main(String[] args) {
+        System.out.println("---Boolean---");
+        System.out.println("Maximo positivo de byte " + Boolean.TRUE);
+        System.out.println("Valor minimo de byte" + Boolean.FALSE);
+
+        boolean bolean = false;
+        if(bolean){
+            System.out.println("La variable contiene un valor verdadero");
+        }
+        else {
+            System.out.println("La variable tiene un valor falso");
+        }
+
+        var edad = 18;
+        var adulto = edad>=18;
+        if(adulto){
+            System.out.println("\n"+"Eres mayor de edad puede pasar");
+        }
+        else {
+            System.out.println("\n"+"Eres menor de edad  no puede pasar");
+        }
+
+    }
+}
