cefff96fec024cae0129c79f01b90a43daba78bf
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-09 17:32:32 -0500

    Agregando ejercicios con sentencias de control

---
+++ EjerciciosNivel3/sentenciasDeControl/src/main/java/com/sentencias/sentenciasDeControl/SentenciasDeControlApplication.java
@@ -0,0 +1,13 @@
+package com.sentencias.sentenciasDeControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenciasDeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenciasDeControlApplication.class, args);
+	}
+
+}
---
+++ EjerciciosNivel3/sentenciasDeControl/src/test/java/com/sentencias/sentenciasDeControl/SentenciasDeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentencias.sentenciasDeControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentenciasDeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
