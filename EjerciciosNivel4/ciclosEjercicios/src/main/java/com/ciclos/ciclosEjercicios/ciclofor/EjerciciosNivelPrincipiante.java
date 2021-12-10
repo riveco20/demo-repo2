a21472080b8b7ec6c77ed69414e7439198e38547
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-10 17:24:29 -0500

    Orientacion a objetos

--- EjerciciosNivel4/ciclosEjercicios/src/main/java/com/ciclos/ciclosEjercicios/ciclofor/EjerciciosNivelPrincipiante.java
+++ EjerciciosNivel4/ciclosEjercicios/src/main/java/com/ciclos/ciclosEjercicios/ciclofor/EjerciciosNivelPrincipiante.java
@@ -1,4 +1,45 @@
 package com.ciclos.ciclosEjercicios.ciclofor;
 
+import java.util.Scanner;
+
 public class EjerciciosNivelPrincipiante {
-}
+    public static void main(String[] args) {
+        System.out.println("Comprobar si un numero es par e impar");
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Ingrese el numero hasta donde quiere calcular los par e impar");
+        int numero = consola.nextInt();
+    /**    for (int i = 0; i <= numero; i++) {
+            var resultado = (i % 2 == 0) ? " es par " : " es impar";
+            System.out.println( i + resultado);
+        }
+
+        for (int i = 0; i <=numero ; i++) {
+            if(numero%2==0){
+                System.out.println("Hola todo va bien");
+            }
+            break;
+        }
+
+        System.out.println("Ocurrio un error se encontro un numero impar");
+
+     */
+
+        for (int i = 0; i <=numero ; i++) {
+            if(numero%2==0){
+
+                System.out.println(i + "Numero par");
+                continue;
+
+            }
+            else {
+                System.out.println(i + "Numero impar");
+            }
+
+        }
+
+        System.out.println("Fin del ciclo");
+
+    }
+
+
+    }
--- EjerciciosNivel4/ciclosEjercicios/src/main/java/com/ciclos/ciclosEjercicios/ciclowhile/EjerciciosNivelPrincipiantes.java
+++ EjerciciosNivel4/ciclosEjercicios/src/main/java/com/ciclos/ciclosEjercicios/ciclowhile/EjerciciosNivelPrincipiantes.java
@@ -1,4 +1,18 @@
 package com.ciclos.ciclosEjercicios.ciclowhile;
 
+import java.util.Scanner;
+
 public class EjerciciosNivelPrincipiantes {
+    public static void main(String[] args) {
+        System.out.println("Comprobar si un numero es par e impar");
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Ingrese el numero hasta donde quiere calcular los par e impar");
+        int numero = consola.nextInt();
+        int contador=0;
+        while (contador<=numero){
+            var resultado = (contador%2==0) ?" es par":" es impar";
+            System.out.println(contador + resultado  );
+            contador++;
+        }
+    }
 }
