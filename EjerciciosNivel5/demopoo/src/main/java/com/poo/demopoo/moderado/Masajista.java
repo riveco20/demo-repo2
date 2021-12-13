56e22763ded7d426d5db067d31658b4ea78f7f4f
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-13 10:06:11 -0500

    Agregando clases hijas de futbol

--- EjerciciosNivel5/demopoo/src/main/java/com/poo/demopoo/moderado/Masajista.java
+++ EjerciciosNivel5/demopoo/src/main/java/com/poo/demopoo/moderado/Masajista.java
@@ -1,4 +1,21 @@
 package com.poo.demopoo.moderado;
 
-public class Masajista {
+public class Masajista extends Futbol{
+    public String titulacion;
+    public Integer añosExperiencia;
+
+    public Masajista(String titulacion, Integer añosExperiencia) {
+        this.titulacion = titulacion;
+        this.añosExperiencia = añosExperiencia;
+    }
+
+    public Masajista(Integer id, String nombre, String apellido, Integer edad, String titulacion, Integer añosExperiencia) {
+        super(id, nombre, apellido, edad);
+        this.titulacion = titulacion;
+        this.añosExperiencia = añosExperiencia;
+    }
+
+    public void darMasaje(){
+        System.out.println("Se esta dando masaje");
+    }
 }
