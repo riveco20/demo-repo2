6efb8c856a72e5025f6fed4661c267f8afafbbc1
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-13 10:40:53 -0500

    Agregar entrenador

--- EjerciciosNivel5/demopoo/src/main/java/com/poo/demopoo/moderado/Entrenador.java
+++ EjerciciosNivel5/demopoo/src/main/java/com/poo/demopoo/moderado/Entrenador.java
@@ -1,4 +1,22 @@
 package com.poo.demopoo.moderado;
 
-public class Entrenador {
+public class Entrenador extends Futbol{
+    public String idFederacion;
+
+    public Entrenador(String idFederacion) {
+        this.idFederacion = idFederacion;
+    }
+
+    public Entrenador(Integer id, String nombre, String apellido, Integer edad, String idFederacion) {
+        super(id, nombre, apellido, edad);
+        this.idFederacion = idFederacion;
+    }
+
+    public void dirigirEntrenamiento(){
+        System.out.println("Entrenamiento Dirigido");
+    }
+
+    public void dirigirPartido(){
+        System.out.println("Partido Dirigido");
+    }
 }
